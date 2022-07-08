9ff7a507fafb0cf2ef24fadf5274e87569c86c79
dassaev-lima <dassaevnascimento@gmail.com>
2022-07-08 16:44:04 -0300

    feat:criando um teste para quando for inputado um string vazia

--- src/tests/test_game.py
+++ src/tests/test_game.py
@@ -5,6 +5,7 @@ class TestGame(unittest.TestCase):
 
     def test_start(self):
         game = Game()
+        self.assertEqual(game.start(''), 'You not walked, but captured 1 pokemon in your current position')
         self.assertEqual(game.start('L'), 2)
         self.assertEqual(game.start('NLSO'), 4)
         self.assertEqual(game.start('NSNSNSNSNS'), 2)
