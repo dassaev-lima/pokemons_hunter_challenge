d9a3f2b9a564cebda0489959c2d0095fdeccc8b6
dassaev-lima <dassaevnascimento@gmail.com>
2022-07-08 01:02:42 -0300

    feat: criando classe game que isola o comportamento de incializar o jogo, retirando a responsabilidade da classe Ash

---
+++ src/models/game.py
@@ -0,0 +1,11 @@
+from .ash import Ash
+
+class Game:
+
+    def start(self,route):
+        ash = Ash()
+        for direction in route.upper():
+            current_size_list_visited_houses, actual_position = ash.walk(direction)
+            ash.has_pokemon(current_size_list_visited_houses)
+    
+        return ash.captured_pokemons
