0898f821f536139ff6182434e8038e3c2110ea1e
dassaev-lima <dassaevnascimento@gmail.com>
2022-07-08 16:45:18 -0300

    feat: adicionando condição pra verificar se a rota digitada é vazia

--- src/models/game.py
+++ src/models/game.py
@@ -3,9 +3,13 @@ from .ash import Ash
 class Game:
 
     def start(self,route):
-        ash = Ash()
-        for direction in route.upper():
-            current_size_list_visited_houses, current_position = ash.walk(direction)
-            ash.has_pokemon(current_size_list_visited_houses)
-    
-        return ash.captured_pokemons
+        if route == '':
+            return 'You not walked, but captured 1 pokemon in your current position'
+        else:
+            ash = Ash()
+            for direction in route.upper():
+                current_size_list_visited_houses, current_position = ash.walk(direction)
+                ash.has_pokemon(current_size_list_visited_houses)
+
+            return ash.captured_pokemons
+            """ print(f'The Ash captured {ash.captured_pokemons} pokemons') """
