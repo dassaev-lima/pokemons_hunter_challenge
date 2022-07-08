477b37d576dd1c4a7941b56da02459a9bb967131
dassaev-lima <dassaevnascimento@gmail.com>
2022-07-08 01:03:41 -0300

    refactor: minimizando a responsabilidade do arquivo main para apenas executar a chamada da classe game e do método start dela

--- src/main.py
+++ src/main.py
@@ -1,18 +1,8 @@
 import time as time
+from models.game import Game
 from models.ash import Ash
-
 if __name__ == "__main__":
-    ash = Ash()
-    ash.pokemon_houses_visited.add(tuple(ash.actual_position))
-    moviments = input("input the moviments made by Ash: \n")
-
-    initial_time = time.time()
-    for direction in moviments.upper():
-        actual_size_list_houses_visitadas, actual_position = ash.to_walk(direction)
-        ash.has_pokemon(actual_size_list_houses_visitadas)
-    
-    print(f'The Ash captured {ash.captured_pokemons} pokemons')
-    final_time = time.time() - initial_time
-
-    """ For check the time of execution descoment the line bottom """
-    """ print(f'Final time {final_time}') """
+    route = input("input the route made by Ash: \n")
+    game = Game()
+    captured_pokemons = game.start(route)
+    print(f'The Ash captured {captured_pokemons} pokemons')
