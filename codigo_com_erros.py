c4a19f71b727387cb832b2d93a0526d7c1700b22
Dassaev Lima <dassaevnascimento@gmail.com>
2024-08-16 09:09:18 +0000

    fix: corrigindo código com erros de padrão de codificação

--- codigo_com_erros.py
+++ codigo_com_erros.py
@@ -1,14 +1,19 @@
-def soma(a,b):return a+b
+def soma(a, b):
+    return a + b
 
-def multiplica(a,b):
-  return a *   b
-  
-def main( ):
-   x=10
-   y= 5
-   resultado_soma= soma(x,y) 
-   resultado_multiplicacao =multiplica(x,y)
-   print(f"A soma de {x} e {y} é: {resultado_soma}") 
-   print(f"A multiplicação de {x} e {y} é: {resultado_multiplicacao}") 
 
-if __name__ == "__main__":main()
+def multiplica(a, b):
+    return a * b
+
+
+def main():
+    x = 10
+    y = 5
+    resultado_soma = soma(x, y)
+    resultado_multiplicacao = multiplica(x, y)
+    print(f"A soma de {x} e {y} é: {resultado_soma}")
+    print(f"A multiplicação de {x} e {y} é: {resultado_multiplicacao}")
+
+
+if __name__ == "__main__":
+    main()
