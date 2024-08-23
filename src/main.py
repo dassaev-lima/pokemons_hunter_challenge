6ec9689972cf0cafcc009f555a0f2eb2fa900289
Dassaev Lima <dassaevnascimento@gmail.com>
2024-08-23 11:24:00 +0000

    fix: Resolvendo problemas de code smell: docstring, imports, linha final

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-import time as time
 from models.game import Game
-from models.ash import Ash
+
 if __name__ == "__main__":
+    """
+    Inclui as classes e chama as funções para inicialização do projeto.
+    """
     route = input("input the route made by Ash: \n")
     game = Game()
     print(game.start(route))
+    
