bc6be138ee1088e0de3cbd31d16547f5ac6a40f7
Dassaev Lima <dassaevnascimento@gmail.com>
2024-08-23 11:27:56 +0000

    fix: Corrigindo code smell de docstring do módulo e importação da classe game

--- src/main.py
+++ src/main.py
@@ -1,4 +1,25 @@
-from models.game import Game
+"""
+Este módulo é o ponto de entrada para o projeto e gerencia a execução do jogo.
+
+Quando executado, o script solicita ao usuário que insira uma rota, cria uma
+instância da classe `Game` importada de `models.game`, e inicia o jogo com a rota
+fornecida. O resultado do jogo é então exibido no console.
+
+Uso:
+    Execute o script para iniciar o jogo e fornecer uma rota.
+
+Exemplo:
+    python main.py
+
+O módulo espera que a classe `Game` esteja definida em `models` e que tenha
+um método `start` que aceite uma rota como argumento.
+
+Autor: Dassaev Lima
+Data: 23/08/2024
+Versão: 1.0
+"""
+
+from models import Game
 
 if __name__ == "__main__":
     """
@@ -7,4 +28,3 @@ if __name__ == "__main__":
     route = input("input the route made by Ash: \n")
     game = Game()
     print(game.start(route))
-    
