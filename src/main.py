91c4a060e357964efff30522b4b2628d57994359
Dassaev Lima <dassaevnascimento@gmail.com>
2024-08-23 11:32:12 +0000

    fix: Corrigindo code smell: Unable to import 'models'

--- src/main.py
+++ src/main.py
@@ -19,12 +19,9 @@ Data: 23/08/2024
 Versão: 1.0
 """
 
-from models import Game
+from models.game import Game
 
 if __name__ == "__main__":
-    """
-    Inclui as classes e chama as funções para inicialização do projeto.
-    """
     route = input("input the route made by Ash: \n")
     game = Game()
     print(game.start(route))
