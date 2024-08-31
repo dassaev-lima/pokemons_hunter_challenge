5b7b81e0f3f0bf483c2d1a3ffeeb356acae717f2
Dassaev Lima <dassaevnascimento@gmail.com>
2024-08-31 21:02:16 +0000

    fix: Corrigir erros de padrão de codificação
    Esse commit adiciona espaçamentos entre as funções
    , entre os parâmetros das funções e uma linha em
    branco no final do arquivo

--- src/models/ash.py
+++ src/models/ash.py
@@ -4,8 +4,8 @@ class Ash:
         self.houses_visited = {(0, 0)}
         self.current_position = [0, 0]
         self.list_size_visited_houses = 1
-        
-    def walk(self,direction):
+
+    def walk(self, direction):
         """walk a house"""
         if direction in 'NSLO':
             if direction == 'N' or direction == 'S':
@@ -13,7 +13,7 @@ class Ash:
                     self.current_position[1] += 1
                 else:
                     self.current_position[1] -= 1
-                
+
                 self.houses_visited.add(tuple(self.current_position))
             else:
                 if direction == 'L':
@@ -21,19 +21,17 @@ class Ash:
                 else:
                     self.current_position[0] -= 1
                 self.houses_visited.add(tuple(self.current_position))
-        
+
         return len(self.houses_visited), self.current_position
 
-    def has_pokemon(self,current_size_list_visited_houses):
-        """ Check if it exists in actual position """
+    def has_pokemon(self, current_size_list_visited_houses):
+        """Check if it exists in actual position"""
         if current_size_list_visited_houses > self.list_size_visited_houses:
             self.capture_pokemon()
             self.list_size_visited_houses = current_size_list_visited_houses
-        
+
         return self.captured_pokemons
-    
+
     def capture_pokemon(self):
-        """ Increase one in quantity of captured pokemons """
+        """Increase one in quantity of captured pokemons"""
         self.captured_pokemons += 1
-    
-    
