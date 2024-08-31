7df54c77108c899f835728a5614e6e36edc875be
Dassaev Lima <dassaevnascimento@gmail.com>
2024-08-31 20:59:23 +0000

    fix: Corrigir erros de codesmells
    Esse commit adiciona espaçamento após as
    importações, uma quebra de linha da função
    assertEqual para melhorar visualização e adiciona
    um linha em branco no fim do arquivo

--- src/tests/test_game.py
+++ src/tests/test_game.py
@@ -1,11 +1,15 @@
 import unittest
 from src.models.game import Game
 
+
 class TestGame(unittest.TestCase):
 
     def test_start(self):
         game = Game()
-        self.assertEqual(game.start(''), 'You not walked, but captured 1 pokemon in your current position')
+        self.assertEqual(
+            game.start(''),
+            'You not walked, but captured 1 pokemon in your current position',
+        )
         self.assertEqual(game.start('L'), 2)
         self.assertEqual(game.start('NLSO'), 4)
         self.assertEqual(game.start('NSNSNSNSNS'), 2)
