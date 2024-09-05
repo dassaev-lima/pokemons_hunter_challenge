4475032f95d3aab5daa6ea4d288f1f8261fa6793
Dassaev Lima <dassaevnascimento@gmail.com>
2024-09-05 04:55:21 +0000

    fix: Remover variável local não utilizada

--- src/models/game.py
+++ src/models/game.py
@@ -9,7 +9,7 @@ class Game:
         else:
             ash = Ash()
             for direction in route.upper():
-                current_size_list_visited_houses, current_position = ash.walk(direction)
+                current_size_list_visited_houses = ash.walk(direction)
                 ash.has_pokemon(current_size_list_visited_houses)
 
             return ash.captured_pokemons
